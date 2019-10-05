c636bc0adeca26684e7eb9ad57d1f0fffdf8d9e2
Nathan Chmielewski <njchimmy@gmail.com>
2019-10-05 16:58:52 -0500

    app-component renders articles[] array property by creating an  app-article element/component for each article. AddArticle() method is called to push a new Article object to the array when user fills form and clicks Submit Link button. Each article row style is defined by app-article component html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,19 @@ import { Article } from './article/article.model';
 export class AppComponent {
   articles: Article[];
 
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http::/angular.io', 10),
+      new Article('Fullstack', 'http://fullstack.io', 5),
+      new Article('ng book', 'http://ng-book.com/2', 1)
+  ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
 }
